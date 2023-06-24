36ede7a33e391022b49d51f71c425aeb72360408
김재령 <wofud0321@naver.com>
2023-06-24 10:49:13 +0900

    TEST

---
+++ 1DAY.py
@@ -0,0 +1,17 @@
+# a 와 b 출력하기
+#a, b = map(int, input().strip().split(' '))
+#print("a =", a, "\nb =", b)
+
+# 문자열 반복해서 출력하기
+# a, b = input().strip().split(' ')
+# b = int(b)
+
+# if len(a) < 1 or len(a) > 10:
+#     a = input().split(' ')
+# if b < 1 or b > 5:
+#     b = input()
+
+# print (a * b)
+
+# 대소문자 바꿔서 출력하기
+str = input()
