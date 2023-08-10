90a5488848e6950d00376613b74d26e057bc2302
김재령 <wofud0321@naver.com>
2023-08-11 08:30:30 +0900

    206

--- LEVEL0/206-215.py
+++ LEVEL0/206-215.py
@@ -1,1 +1,18 @@
-print(solution( ["right", "right", "right", "right", "right", "left"], [9, 5]))
+# 206. 옹알이
+# def solution(babbling):
+#     words = ["aya", "ye", "woo", "ma"]
+#     answer = 0
+#     for i in babbling:
+#         temp = i
+#         for word in words:
+#             if word in temp:
+#                 temp = temp.replace(word,'*')
+        
+#         temp = temp.replace('*','')
+#         if temp == '': answer+=1
+    
+#     return answer
+
+# 207. k의 개수
+
+print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
