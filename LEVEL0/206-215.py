8b0fe7ec6b631dcefbc8b9cabc4322bf485d82d1
김재령 <wofud0321@naver.com>
2023-08-12 12:41:41 +0900

    208

--- LEVEL0/206-215.py
+++ LEVEL0/206-215.py
@@ -14,5 +14,39 @@
 #     return answer
 
 # 207. k의 개수
+# def solution(i, j, k):
+#     li=[]
+#     for num in range(i,j+1):
+#         for num2 in str(num):
+#             li.append(num2)
 
+#     return li.count(str(k))
+
+# 더 괜찮은 답변
+# def solution(i, j, k):
+#     answer = sum([ str(i).count(str(k)) for i in range(i,j+1)])
+#     return answer
+
+# 208. 배열의 유사도
+# def solution(s1, s2):
+#     answer = 0
+#     for check in s1:
+#         for check2 in s2:
+#             if check == check2:
+#                 answer += 1
+#     return answer
+
+# 집합으로 표현
+# def solution(s1, s2):
+#     answer = 0
+
+#     for word in s1:
+#         if word in s2:
+#             answer += 1
+#         else:
+#             continue
+
+#     return answer
+
+# 209.
 print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
