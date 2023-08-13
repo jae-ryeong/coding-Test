9aca3ad60458b7725e36f24144bb07f983682dbd
김재령 <wofud0321@naver.com>
2023-08-13 09:56:30 +0900

    211

--- LEVEL0/206-215.py
+++ LEVEL0/206-215.py
@@ -48,5 +48,48 @@
 
 #     return answer
 
-# 209.
-print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
+# 209. 외계어 사전
+# def solution(spell, dic):
+#     for i in dic:
+#         check = 0
+#         for j in spell:
+#             if j in i:
+#                 check+=1
+#         if check == len(spell):
+#             return 1
+#     return 2
+
+# 집합 활용
+# def solution(spell, dic):
+#     spell = set(spell)
+#     for s in dic:
+#         if not spell-set(s):
+#             return 1
+#     return 2
+
+# 210. 잘라서 배열로 저장하기
+# def solution(my_str, n):
+#     answer = []
+#     a=0
+#     for i in range(n,len(my_str),n):
+#         answer.append(my_str[a:i])
+#         a+=n
+    
+#     if len(my_str) > a:
+#         answer.append(my_str[a:])
+#     return answer
+
+# 슬라이싱은 초과해도 에러가 나지 않는다 ㅜㅜ
+# def solution(my_str, n):
+#     return [my_str[i:i+n] for i in range(0,len(my_str),n)]
+
+# 211. 7의 개수
+# def solution(array):
+#     return sum([str(i).count("7") for i in array])
+
+# 더 쉽게
+# def solution(array):
+#     return str(array).count('7')
+
+# 212. 
+print(solution([7, 77, 17]))
