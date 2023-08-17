ee91b0d8a3a61cb9d6d38fdd8dc2bdaed573c8da
김재령 <wofud0321@naver.com>
2023-08-18 08:50:00 +0900

    218

--- LEVEL0/216-225.py
+++ LEVEL0/216-225.py
@@ -17,6 +17,26 @@
 #     else:
 #         return 0
 
-# 217. 
+# 217. 가장 큰 수 찾기
+# def solution(array):
+#     answer = [max(array)]
+#     answer.append(array.index(answer[0]))
+#     return answer
 
+# def solution(array):
+#     return [max(array), array.index(max(array))]
+
+# 218. 문자열안에 문자열
+# def solution(str1, str2):
+#     return 1 if str2 in str1 else 2
+
+# 219. 제곱수 판별하기
+# import math
+# def solution(n):
+#     return 1 if math.sqrt(n) % 1 == 0 else 2
+
+# def solution(n):
+#     return 1 if (n ** 0.5).is_integer() else 2
+
+# 220.
 print(solution("aAb1B2cCB34oOp"))
