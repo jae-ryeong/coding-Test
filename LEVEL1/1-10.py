16359ff6926ee7ac392e989cb3957ed7f268945c
김재령 <wofud0321@naver.com>
2023-08-23 00:08:49 +0900

    7 까지

--- LEVEL1/1-10.py
+++ LEVEL1/1-10.py
@@ -42,5 +42,50 @@
 #                 b.append(j)
 #     return [min(a), min(b), max(a) + 1, max(b) + 1]
 
-# 5. 
-print(solution([".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."]))
+# 5. 자릿수 더하기
+# def solution(n):
+#     return sum(int(i) for i in str(n))
+
+# 6. 제일 작은 수 제거하기
+# def solution(arr):
+#     min_arr = min(arr)
+#     if len(arr) == 1:
+#         return [-1]    
+#     return [i for i in arr if i!=min_arr]
+# 변수를 사용하지 않으면 시간복잡도가 증가해 에러가 난다. 왜인지 모름
+
+# 7. 둘만의 암호
+# def solution(s, skip, index):
+#     answer = []
+#     skip_ord = [ord(i) for i in skip]
+    
+#     for i in s:
+#         check_ord = ord(i)
+#         for _ in range(index):
+#             check_ord = check_ord + 1
+#             if check_ord > 122: check_ord = check_ord-26
+#             while(check_ord in skip_ord):
+#                 check_ord = check_ord + 1
+#                 if check_ord > 122: check_ord = check_ord-26
+#             if check_ord > 122: check_ord = check_ord-26
+#         answer.append(chr(check_ord))    
+
+#     return ''.join(answer)
+
+# from string import ascii_lowercase
+
+# def solution(s, skip, index):
+#     result = ''
+
+#     a_to_z = set(ascii_lowercase)
+#     a_to_z -= set(skip)
+#     a_to_z = sorted(a_to_z)
+#     l = len(a_to_z)
+
+#     dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
+#     for i in s:
+#         result += a_to_z[(dic_alpha[i] + index) % l]
+
+#     return result
+print(solution("y", "baz", 1))
