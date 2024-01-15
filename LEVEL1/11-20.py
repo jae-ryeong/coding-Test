6e813adaa85b1109202b3ec71d3498d9300f89c1
김재령 <wofud0321@naver.com>
2024-01-15 22:47:03 +0900

    01/15

--- LEVEL1/11-20.py
+++ LEVEL1/11-20.py
@@ -137,4 +137,20 @@
 
 #     return answer
 
-print(solution(3,20,4))
+#20. K번째 수
+# def solution(array, commands):
+#     answer = []
+#     for i in range(len(commands)):
+#         temp = []
+#         temp.append(sorted(array[commands[i][0]-1 : commands[i][1]: ]))
+#         answer.append(temp[0][commands[i][2]-1])
+#     return answer
+
+# # 더 좋은 방법
+# def solution(array, commands):
+#     answer = []
+#     for command in commands:
+#         i,j,k = command
+#         answer.append(list(sorted(array[i-1:j]))[k-1])
+#     return answer
+#print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
