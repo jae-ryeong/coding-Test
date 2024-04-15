a17e32f068eb9b47ee2b0fe5e234eb3764b6c842
김재령 <wofud0321@naver.com>
2024-04-15 10:33:06 +0900

    04/15

--- LEVEL1/21-30.py
+++ LEVEL1/21-30.py
@@ -1,4 +1,16 @@
 # 21. 문자열 내림차순으로 배치하기
-def solution(s):
-    answer = ''.join(sorted(s)[::-1])
-    return answer
+# def solution(s):
+#     answer = ''.join(sorted(s)[::-1])
+#     return answer
+
+# 22. 문자열 내 p와 y의 개수
+# def solution(s):
+#     word = s.upper()
+
+#     nump = word.count('P')
+#     numy = word.count('Y')
+
+#     if nump == numy: return True
+#     else: return False
+
+# print(solution("pPoooyY"))
